22534b12020876c03020f1a405a97613900b2e69
MoXiaoJian <1349692682@qq.com>
2018-06-04 21:15:52 +0800

    Create demo1.php

---
+++ PHP/demo1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+	form
+	{
+		height: 500px;
+		width: 500px;
+	}
+
+	</style>
+</head>
+<body>
+	<h1>demo1<h1>
+        
+	<form action="./demo1.php" method='post'>
+	
+        <input type="text" name="number1" value="">
+	<br>
+	<input type="submit" name="Rhombus" value="菱形">
+	<input type="submit" name="Pyramid" value="金字塔">
+
+	</form>
+        <?php
+            
+        ?>
+
+</body>
+</html>
